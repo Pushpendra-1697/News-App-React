6d50eb907d72dcb1c6787df2390ed7c34b7923c4
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2023-08-01 01:10:09 +0530

    added sorting and filtering in every thing route

--- myapp/src/Components/Everything.jsx
+++ myapp/src/Components/Everything.jsx
@@ -1,7 +1,8 @@
-import { Box, Button, Heading, Text, Img, Input, useToast } from '@chakra-ui/react';
+import { Box, Button, Heading, Text, Img, Input, useToast, Select } from '@chakra-ui/react';
 import React, { useState } from 'react'
 import Loading from './Loading';
 import { Link } from 'react-router-dom';
+import Error from './Error';
 
 const Everything = () => {
     const [value, setValue] = useState([]);
@@ -11,10 +12,12 @@ const Everything = () => {
     const toast = useToast();
     const [toggler, setToggler] = useState(false);
 
-    const getData = async () => {
+
+    const getData = async (e) => {
+        let sort = e.target.value;
         try {
             setLoading(true);
-            let res = await fetch(`https://newsapi.org/v2/everything?q=${title}&apiKey=b3ec9c97fea04373914caf3c7dbe5604`);
+            let res = await fetch(`https://newsapi.org/v2/everything?q=${title}&sortBy=${sort}&apiKey=32b6a06a030e40e5a220e39c280795f2`);
             res = await res.json();
             setLoading(false);
             if (res.status == 'error') {
@@ -35,32 +38,40 @@ const Everything = () => {
         setToggler(!toggler);
     };
 
-    console.log(value);
 
     return (
         <Box width={'90%'} m='auto'>
-            <Box mb='5%' display={'flex'} justifyContent={'space-around'}>
+            <Box m='5%' display={'flex'} justifyContent={'space-around'}>
                 <Box>
                     <Input width={'200px'} onChange={(e) => setTitle(e.target.value)} placeholder='Enter Yr Title' />
                     <Button onClick={getData}>{loading ? <Loading /> : "Search"}</Button>
                 </Box>
-                <Button onClick={handleToggler}>Toggler</Button>
+                <Box display={'flex'} gap={'5px'} flexDirection={['column', 'column', 'row']}>
+                    <Select onChange={getData}>
+                        <option value={''}>SORT BY</option>
+                        <option value={'popularity'}>Popularity</option>
+                        <option value={'publishedAt'}>PublishedAt</option>
+                        <option value={'relevancy'}>Relevancy</option>
+                    </Select>
+                    <Button onClick={handleToggler}>Toggler</Button>
+                </Box>
             </Box>
+            {error && <Error />}
 
             {
-                value.length == 0 ? <Heading>No Search Till Now</Heading> :
+                value.length === 0 ? <Heading color={'goldenrod'}>No Search Till Now 😊</Heading> :
 
-                    <Box display={'grid'} gridTemplateColumns={toggler ? 'repeat(2,1fr)' : 'repeat(3,1fr)'} gap={'20px'}>
+                    <Box display={'grid'} gridTemplateColumns={[toggler ? 'repeat(2,1fr)' : 'repeat(1,1fr)', toggler ? 'repeat(2,1fr)' : 'repeat(1,1fr)', toggler ? 'repeat(2,1fr)' : 'repeat(3,1fr)']} gap={'20px'}>
                         {value && value.map(({ urlToImage, source, title, description, author, publishedAt, url
-                        }) =>
-                            <Box padding={'3px'} boxShadow="rgba(100, 100, 111, 0.2) 0px 7px 29px 0px" borderRadius={'10px'} key={source.id
+                        }, index) =>
+                            <Box padding={'3px'} boxShadow="rgba(100, 100, 111, 0.2) 0px 7px 29px 0px" borderRadius={'10px'} key={index
                             }>
                                 <Img borderRadius={'5px'} mb='2%' w={'100%'} h='250px' src={urlToImage} alt={title} />
                                 <Text mb='2%' lineHeight={'16px'}>Title: {title}</Text>
                                 <Text mb='2%'>Author: {author}</Text>
                                 <Text lineHeight={'16px'} mb='2%'>Description: {description}</Text>
                                 <Text mb='2%'>PublishedAt: {publishedAt}</Text>
-                                <Text fontSize={'23px'} color={'red'}><Link to={url}>MORE</Link></Text>
+                                <Text fontSize={'23px'} color={'red'}><Link target='_blank' to={url}>MORE</Link></Text>
                             </Box>
                         )}
                     </Box>
